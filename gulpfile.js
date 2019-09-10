3b643a289ef29e964e307cd2594cf37a2818368c
denizerisen <38030540+denizerisen@users.noreply.github.com>
2019-09-10 21:50:26 +0300

    fixed layout, icon

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 function style(){
-    return gulp.src('./src/sass/**/*.sass')
+    return gulp.src('./src/sass/main.sass')
     .pipe(sass())
     .pipe(gulp.dest('./src/css'))
 }
--- src/components/TrialInfo/TrialInfo.js
+++ src/components/TrialInfo/TrialInfo.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function ButtonTrial(){
+function TrialInfo(){
     
-  return  <div className="trial-cta">  
+  return  <div className="trial-short-info">  
             <span>Try it free 7 days</span> then $20/mo. thereafter
           </div>
 }
-export default ButtonTrial;
+export default TrialInfo;
 
